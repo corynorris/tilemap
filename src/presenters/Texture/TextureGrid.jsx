9369f91fc7097dee05686ff8d8724c9eb85a4510
Cory Norris <corynorris@gmail.com>
2018-12-29 20:45:32 -0500

    updated create react app to remove vulnerabilities

--- src/presenters/Texture/TextureGrid.jsx
+++ src/presenters/Texture/TextureGrid.jsx
@@ -1,78 +1,82 @@
-import React, { Component } from 'react';
-import Grid from '../Grid';
-import { range } from '../../core/utils.js';
-import { cropImage } from '../../core/image.js';
+import React, { Component } from "react";
+import Grid from "../Grid";
+import { range } from "../../core/utils.js";
+import { cropImage } from "../../core/image.js";
 
 class TextureGrid extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      images: [[]],
-    }
+      images: [[]]
+    };
   }
 
-  generateImages({ texture, tilesWide, tilesHigh}) {
-    let { dataUri, width, height} = texture;
+  generateImages({ texture, tilesWide, tilesHigh }) {
+    let { dataUri, width, height } = texture;
     const imageWidth = width / tilesWide;
     const imageHeight = height / tilesHigh;
 
     const promises = range(tilesWide).map((row, x) =>
-      Promise.all(range(tilesHigh).map((col, y) => {
-        const cx = x * imageWidth;
-        const cy = y * imageHeight;
-        return cropImage(dataUri, cx, cy, imageWidth, imageHeight).then((croppedImage) => {
-          return croppedImage;
-        });
-      }))
+      Promise.all(
+        range(tilesHigh).map((col, y) => {
+          const cx = x * imageWidth;
+          const cy = y * imageHeight;
+          return cropImage(dataUri, cx, cy, imageWidth, imageHeight).then(
+            croppedImage => {
+              return croppedImage;
+            }
+          );
+        })
+      )
     );
 
     Promise.all(promises).then(images => {
       this.setState({
         images
       });
-    })
+    });
   }
 
   handleDrag(image, key, ev) {
-    ev.dataTransfer.setData('style', JSON.stringify({
-      image,
-      key,
-    }));
+    ev.dataTransfer.setData(
+      "style",
+      JSON.stringify({
+        image,
+        key
+      })
+    );
   }
 
   componentWillReceiveProps(nextProps) {
-    this.generateImages(nextProps)
+    this.generateImages(nextProps);
   }
 
   componentWillMount() {
-    this.generateImages(this.props)
+    this.generateImages(this.props);
   }
 
   render() {
     const { tilesHigh, tileWidth, tileHeight } = this.props;
-    
-    const components = this.state.images.map(
-      (imageRow, x) => imageRow.map(
-        (image, y) => {
-          const key = y * tilesHigh + x;
-          return (
-            <td key={key}>
-              <img
-                onDragStart={this.handleDrag.bind(this, image, key)}
-                src={image}
-                alt={`${name}-${x}-${y}`}
-                style={{
-                  width: tileWidth,
-                  height: tileHeight,
-                }} />
-            </td>
-          );
-        }
-      )
+
+    const components = this.state.images.map((imageRow, x) =>
+      imageRow.map((image, y) => {
+        const key = y * tilesHigh + x;
+        return (
+          <td key={key}>
+            <img
+              onDragStart={this.handleDrag.bind(this, image, key)}
+              src={image}
+              alt={`${key}-${x}-${y}`}
+              style={{
+                width: tileWidth,
+                height: tileHeight
+              }}
+            />
+          </td>
+        );
+      })
     );
-    return (
-      <Grid components={components} />
-    );
+    return <Grid components={components} />;
   }
 }
 
